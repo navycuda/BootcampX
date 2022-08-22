fd409c4a1e0c995ecc022102b6c513a1114f373c
NavyCuda <navysupra@gmail.com>
2022-08-22 16:15:01 -0700

    Where end date

--- query.sql
+++ query.sql
@@ -16,4 +16,5 @@
 SELECT students.name AS student, count(assignment_submissions.*) as total_submissions
 FROM assignment_submissions
 JOIN students ON students.id = assignment_submissions.student_id
+WHERE students.end_date IS NULL
 GROUP BY students.name;
