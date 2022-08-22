c871723c4ea2491feb6373d05f2840e6cd7ca13e
NavyCuda <navysupra@gmail.com>
2022-08-22 15:47:30 -0700

    Corrected to match answer

--- 2_queries_joins/1_total_duration.sql
+++ 2_queries_joins/1_total_duration.sql
@@ -1,4 +1,4 @@
-SELECT SUM(assignment_submissions.duration)
+SELECT sum(assignment_submissions.duration) AS total_duration
 FROM assignment_submissions
 JOIN students ON students.id = assignment_submissions.student_id
 WHERE students.name = "Ibrahim Schimmel";
