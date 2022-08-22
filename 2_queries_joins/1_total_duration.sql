0e1645c205c572972015664567aa5db820365640
NavyCuda <navysupra@gmail.com>
2022-08-22 15:48:33 -0700

    Used correct quotation marks

--- 2_queries_joins/1_total_duration.sql
+++ 2_queries_joins/1_total_duration.sql
@@ -1,4 +1,4 @@
 SELECT sum(assignment_submissions.duration) AS total_duration
 FROM assignment_submissions
 JOIN students ON students.id = assignment_submissions.student_id
-WHERE students.name = "Ibrahim Schimmel";
+WHERE students.name = 'Ibrahim Schimmel';
