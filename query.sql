45e8e7f9f9ffb2d5e737c389483ede2cd692c224
NavyCuda <navysupra@gmail.com>
2022-08-22 15:00:45 -0700

    First outer join

--- query.sql
+++ query.sql
@@ -1,2 +1,5 @@
-SELECT students.name, students.email, cohorts.name 
-FROM students INNER JOIN cohorts ON cohort_id = cohorts.id;
+-- SELECT students.name, students.email, cohorts.name 
+-- FROM students INNER JOIN cohorts ON cohort_id = cohorts.id;
+
+SELECT students.name, students.email, cohorts.name
+FROM students OUTER JOIN cohorts ON cohort_id = cohorts.id;
