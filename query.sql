21ec304c1f66f786853c715fab4c942558141950
NavyCuda <navysupra@gmail.com>
2022-08-22 15:04:24 -0700

    Right outer join

--- query.sql
+++ query.sql
@@ -1,6 +1,14 @@
 -- SELECT students.name, students.email, cohorts.name 
 -- FROM students INNER JOIN cohorts ON cohort_id = cohorts.id;
 
+-- SELECT students.name, students.email, cohorts.name
+-- FROM students LEFT OUTER JOIN cohorts ON cohort_id = cohorts.id
+-- ORDER BY students.name;
+
 SELECT students.name, students.email, cohorts.name
-FROM students LEFT OUTER JOIN cohorts ON cohort_id = cohorts.id
+FROM students RIGHT OUTER JOIN cohorts ON cohort_id = cohorts.id
 ORDER BY students.name;
+
+-- SELECT students.name, students.email, cohorts.name
+-- FROM students FULL OUTER JOIN cohorts ON cohort_id = cohorts.id
+-- ORDER BY students.name;
