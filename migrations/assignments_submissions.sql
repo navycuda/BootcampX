8e4b153ea320f04701b26e1454b77a2d67e79f7a
NavyCuda <navysupra@gmail.com>
2022-08-22 14:19:13 -0700

    Found errors, correct

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -8,7 +8,7 @@ CREATE TABLE assignments (
 );
 CREATE TABLE assignments_submissions (
     id SERIAL PRIMARY KEY NOT NULL,
-    assignment_id INTEGER,
+    assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
     student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
     duration INTEGER,
     submission_date DATE
