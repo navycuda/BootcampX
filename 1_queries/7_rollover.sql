b536a4e01c5f63e649c561d53acde9db16527f6e
NavyCuda <navysupra@gmail.com>
2022-08-22 15:23:31 -0700

    Fix typos, now works

--- 1_queries/7_rollover.sql
+++ 1_queries/7_rollover.sql
@@ -1,8 +1,9 @@
 SELECT 
   students.name AS student_name,
-  cohort.name AS cohort_name,
-  cohort.start_date AS cohort_start_date,
+  cohorts.name AS cohort_name,
+  cohorts.start_date AS cohort_start_date,
   students.start_date AS student_start_date
-FROM students JOIN cohorts ON students.cohort_id = cohorts.id
+FROM students 
+JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE students.start_date != cohorts.start_date
-ORDER BY students.start_date;
+ORDER BY cohorts.start_date;
