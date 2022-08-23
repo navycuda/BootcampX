de0804e134f9d58503d5c517618db5f3e1dd3197
NavyCuda <navysupra@gmail.com>
2022-08-23 16:09:28 -0700

    First attempt

--- 4_queries/5_average_cohort_assistance_time.sql
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -1,3 +1,15 @@
 /*
 \i 4_queries/5_average_cohort_assistance_time.sql
 */
+SELECT 
+  cohorts.name AS name,
+  avg(assistance_requests.completed_at - assistance_requests.started_at) AS average_assistance_time
+FROM 
+  cohorts
+JOIN
+  students
+  ON cohorts.id = students.cohort_id
+JOIN
+  assistance_requests
+  ON students.id = assistance_requests.student_id
+GROUP BY cohorts.name;
