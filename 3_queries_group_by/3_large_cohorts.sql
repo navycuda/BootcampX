c9d42cc27b5893ece461230eaf97b4cb10f3fb18
NavyCuda <navysupra@gmail.com>
2022-08-22 17:24:38 -0700

    Corrected to what should work

--- 3_queries_group_by/3_large_cohorts.sql
+++ 3_queries_group_by/3_large_cohorts.sql
@@ -2,5 +2,5 @@
 SELECT cohorts.name AS cohort_name, count(students.*) AS student_count
 FROM cohorts
 JOIN students ON cohorts.id = students.cohort_id
-HAVING student_count >= 18;
-ORDER BY cohort.name;
+HAVING count(students.*) >= 18
+ORDER BY student_count;
