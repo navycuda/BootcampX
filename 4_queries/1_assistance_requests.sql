9ba49c342a55fdf4ab748d01e541c354749d3d6c
NavyCuda <navysupra@gmail.com>
2022-08-23 14:36:17 -0700

    Fix errors

--- 4_queries/1_assistance_requests.sql
+++ 4_queries/1_assistance_requests.sql
@@ -1,5 +1,6 @@
 -- 4_queries/1_assistance_requests.sql
-SELECT count(assistance_requests) AS total_asistances, teachers.name
+SELECT count(assistance_requests) AS total_assistances, teachers.name
 FROM assistance_requests
 JOIN teachers ON assistance_requests.teacher_id = teachers.id
-WHERE teachers.name = 'Waylon Boehm'; 
+WHERE teachers.name = 'Waylon Boehm' 
+GROUP BY teachers.name;
