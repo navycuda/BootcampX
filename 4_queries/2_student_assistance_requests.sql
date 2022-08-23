47b257e8f855b19d19517026b2bdb7f734028049
NavyCuda <navysupra@gmail.com>
2022-08-23 14:47:52 -0700

    First try at student assistance

--- 4_queries/2_student_assistance_requests.sql
+++ 4_queries/2_student_assistance_requests.sql
@@ -1,3 +1,11 @@
 /*
 \i 4_queries/2_student_assistance_requests.sql
 */
+SELECT 
+  count(assistance_requests) AS total_assistances,
+  students.name AS name
+FROM assistance_requests
+JOIN students
+  ON students.id = assistance_requests.student_id
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name;
