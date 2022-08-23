2d5e6752ea22457facdaa756770d127b9d6d17a7
NavyCuda <navysupra@gmail.com>
2022-08-23 14:16:58 -0700

    Comment out last query

--- query.sql
+++ query.sql
@@ -41,12 +41,12 @@
 -- JOIN students ON students.id = student_id
 -- WHERE students.name = 'Ibrahim Schimmel';
 
-SELECT assignments.name
-FROM assignments
-WHERE id NOT IN
-(
-  SELECT assignment_id
-  FROM assignment_submissions
-  JOIN students ON students.id = student_id
-  WHERE students.name = 'Ibrahim Schimmel'
-);
+-- SELECT assignments.name
+-- FROM assignments
+-- WHERE id NOT IN
+-- (
+--   SELECT assignment_id
+--   FROM assignment_submissions
+--   JOIN students ON students.id = student_id
+--   WHERE students.name = 'Ibrahim Schimmel'
+-- );
