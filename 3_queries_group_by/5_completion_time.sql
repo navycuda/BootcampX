4ba691f374efa34709c454bb6a8e728b99f6ea02
NavyCuda <navysupra@gmail.com>
2022-08-22 18:01:48 -0700

    Fix typo

--- 3_queries_group_by/5_completion_time.sql
+++ 3_queries_group_by/5_completion_time.sql
@@ -1,5 +1,5 @@
 -- 3_queries_group_by/5_completion_time.sql
-SELECT students.name AS student, AVG(assignments.duration) average_assignment_duration
+SELECT students.name AS student, AVG(assignment_submissions.duration) average_assignment_duration
 FROM students
 JOIN assignment_submissions ON students.id = assignment_submissions.student_id
 WHERE students.end_date IS NULL
