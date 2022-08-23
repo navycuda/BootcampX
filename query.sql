b8c82119da2cb22665d23281eceb43608ee5af3a
NavyCuda <navysupra@gmail.com>
2022-08-22 18:26:33 -0700

    Experiments as per instructions

--- query.sql
+++ query.sql
@@ -13,9 +13,17 @@
 -- FROM students FULL OUTER JOIN cohorts ON cohort_id = cohorts.id
 -- ORDER BY students.name;
 
-SELECT students.name AS student, count(assignment_submissions.*) as total_submissions
+-- SELECT students.name AS student, count(assignment_submissions.*) as total_submissions
+-- FROM assignment_submissions
+-- JOIN students ON students.id = assignment_submissions.student_id
+-- WHERE students.end_date IS NULL
+-- GROUP BY students.name
+-- HAVING count(assignment_submissions.*) < 100;
+
+SELECT (
+  SELECT count(assignments)
+  FROM assignments
+)-count(assignment_submissions) as total_incomplete
 FROM assignment_submissions
-JOIN students ON students.id = assignment_submissions.student_id
-WHERE students.end_date IS NULL
-GROUP BY students.name
-HAVING count(assignment_submissions.*) < 100;
+JOIN students ON assignment_submissions.student_id = students.id
+WHERE students.name = 'Ibrahim Schimmel';
