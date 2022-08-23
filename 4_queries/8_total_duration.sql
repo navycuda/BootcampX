3e8beca361075dfd0ce2aa84430b500cc43d25ac
NavyCuda <navysupra@gmail.com>
2022-08-23 16:59:49 -0700

    Wrote first attempt

--- 4_queries/8_total_duration.sql
+++ 4_queries/8_total_duration.sql
@@ -1,3 +1,17 @@
 /*
 \i 4_queries/8_total_duration.sql
 */
+SELECT
+  cohorts.name,
+  sum(assistance_requests.completed_at - assistance_requests.started_at) AS total_duration
+FROM
+  cohorts
+JOIN
+  students
+  ON cohorts.id = students.cohort_id
+JOIN
+  assistance_requests
+  ON students.id = assistance_requests.student_id
+ORDER BY
+  total_duration
+  DESC;
