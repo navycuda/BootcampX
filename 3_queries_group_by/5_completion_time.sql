be4d4b8b09a4831a50ed6f2c0aea40d4258f942c
NavyCuda <navysupra@gmail.com>
2022-08-22 18:01:05 -0700

    Corrected to example

--- 3_queries_group_by/5_completion_time.sql
+++ 3_queries_group_by/5_completion_time.sql
@@ -1,9 +1,7 @@
 -- 3_queries_group_by/5_completion_time.sql
 SELECT students.name AS student, AVG(assignments.duration) average_assignment_duration
 FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
 JOIN assignment_submissions ON students.id = assignment_submissions.student_id
-JOIN assignments ON assignment_submissions.assignment_id = assignments.id
 WHERE students.end_date IS NULL
-GROUP BY students.name
+GROUP BY student
 ORDER BY average_assignment_duration DESC;
