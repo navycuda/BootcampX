cf7b71952fb1d13baf8b751b2f7a8ca5b89ddd46
NavyCuda <navysupra@gmail.com>
2022-08-22 18:13:42 -0700

    Added group by

--- 3_queries_group_by/6_low_completion_time.sql
+++ 3_queries_group_by/6_low_completion_time.sql
@@ -1,9 +1,11 @@
 -- 3_queries_group_by/6_low_completion_time.sql
-SELECT student.name AS student,
+SELECT students.name AS student,
   avg(assignment_submissions.duration) average_assignment_duration,
   avg(assignments.duration) average_estimate_duration
 FROM students
 JOIN assignment_submissions ON students.id = assignment_submissions.student_id
-JOIN assignements ON assignment_submissions.assignment_id = assignments.id
-WHERE avg(assignments.duration) > avg(assignment_submissions.duration)
+JOIN assignments ON assignment_submissions.assignment_id = assignments.id
+WHERE students.end_date IS NULL
+GROUP BY student
+HAVING avg(assignments.duration) > avg(assignment_submissions.duration)
 ORDER BY average_assignment_duration;
