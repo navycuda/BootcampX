3ff64b7f26de301d5edc86ff0a41c77cea3a067b
NavyCuda <navysupra@gmail.com>
2022-08-23 16:15:56 -0700

    One small change

--- 4_queries/6_longest_assistances.sql
+++ 4_queries/6_longest_assistances.sql
@@ -1,3 +1,18 @@
 /*
 \i 4_queries/6_longest_assistances.sql
 */
+SELECT 
+  cohorts.name AS name,
+  max(assistance_requests.completed_at - assistance_requests.started_at) AS average_assistance_time
+FROM 
+  cohorts
+JOIN
+  students
+  ON cohorts.id = students.cohort_id
+JOIN
+  assistance_requests
+  ON students.id = assistance_requests.student_id
+GROUP BY 
+  cohorts.name
+ORDER BY
+  average_assistance_time;
