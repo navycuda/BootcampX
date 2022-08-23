d6f5808f2118b71771c7fbd5ae1a89694c5d7c45
NavyCuda <navysupra@gmail.com>
2022-08-22 17:28:56 -0700

    Trying variant of previous example

--- 4_total_submissions.sql
+++ 4_total_submissions.sql
@@ -1,1 +1,7 @@
 -- 4_total_submissions.sql
+SELECT cohorts.name AS cohort, count(students.*) AS student_count
+FROM cohorts
+JOIN students ON cohorts.id = students.cohort_id
+GROUP BY cohort_name
+HAVING count(students.*) >= 18
+ORDER BY student_count;
