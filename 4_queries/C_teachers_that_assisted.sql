2077da03908271df8d14eeda71545eae0ad7ab55
NavyCuda <navysupra@gmail.com>
2022-08-23 19:53:38 -0700

    Add working code from example

--- 4_queries/C_teachers_that_assisted.sql
+++ 4_queries/C_teachers_that_assisted.sql
@@ -19,3 +19,21 @@ WHERE
 GROUP BY
   cohorts.name;
 */
+SELECT DISTINCT
+  teachers.name AS teacher,
+  cohorts.name as cohort_id
+FROM
+  teachers
+JOIN
+  assistance_requests
+  ON teachers.id = assistance_requests.teacher_id
+JOIN
+  students
+  ON  assistance_requests.student_id = students.id
+JOIN
+  cohorts
+  ON  students.cohort_id = cohorts.id
+WHERE
+  cohorts.name = 'JUL02'
+ORDER BY
+  teacher;
