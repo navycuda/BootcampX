a33e09d51b01bcac7da9e3dbc7eaef20fa3852ad
NavyCuda <navysupra@gmail.com>
2022-08-23 19:17:03 -0700

    Add correct solution

--- 4_queries/A_most_confusing_assignments.sql
+++ 4_queries/A_most_confusing_assignments.sql
@@ -32,3 +32,18 @@ FROM (
 ) AS total_durations;
 */
 
+SELECT
+  assignments.id,
+  assignments.name,
+  assignments.day,
+  assignmnets.chapter,
+  count(assistance_requests) AS total_requests
+FROM
+  assignments
+JOIN
+  assistance_requests
+  ON assignments.id = assistance_requests.assignment_id
+GROUP BY
+  assignments.id
+ORDER BY
+  total_requests DESC;
