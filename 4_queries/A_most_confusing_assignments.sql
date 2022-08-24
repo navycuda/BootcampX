e448668f1ea4a2d00154c03afb654339906b4542
NavyCuda <navysupra@gmail.com>
2022-08-23 19:09:09 -0700

    Clean up

--- 4_queries/A_most_confusing_assignments.sql
+++ 4_queries/A_most_confusing_assignments.sql
@@ -6,4 +6,25 @@ SELECT
   assignments.name,
   assignments.day,
   assignments.chapter,
-  
+  count(total_duration) AS total_requests
+FROM (
+  SELECT
+  avg(total_duration) AS average_total_duration
+  FROM (
+    SELECT
+      cohorts.name AS cohort,
+      sum(assistance_requests.completed_at-assistance_requests.started_at) AS total_duration
+    FROM
+      assistance_requests
+    JOIN
+      students
+      ON assistance_requests.student_id = students.id
+    JOIN
+      cohorts
+      ON students.cohort_id = cohorts.id
+    GROUP BY
+      cohorts.name
+    ORDER BY
+      total_duration
+  ) AS total_durations
+) AS total_durations
