211f7492c04ad9c40d10ea3638d44058d06bcb21
NavyCuda <navysupra@gmail.com>
2022-08-23 20:07:18 -0700

    Add count()

--- 4_queries/D_teachers_number_assistances.sql
+++ 4_queries/D_teachers_number_assistances.sql
@@ -1,3 +1,22 @@
 /*
 \i 4_queries/D_teachers_number_assistances.sql
 */
+SELECT DISTINCT
+  teachers.name AS teacher,
+  cohorts.name as cohort,
+  count(assistance_requests.*)
+FROM
+  teachers
+JOIN
+  assistance_requests
+  ON teachers.id = assistance_requests.teacher_id
+JOIN
+  students
+  ON  assistance_requests.student_id = students.id
+JOIN
+  cohorts
+  ON  students.cohort_id = cohorts.id
+WHERE
+  cohorts.name = 'JUL02'
+ORDER BY
+  teacher;
