c0aee9cd03afd3a9ee500e2b97695b3e056e6811
NavyCuda <navysupra@gmail.com>
2022-08-23 19:24:01 -0700

    First attempt

--- 4_queries/B_total_assignments_and_duration.sql
+++ 4_queries/B_total_assignments_and_duration.sql
@@ -1,3 +1,11 @@
 /*
 \i 4_queries/B_total_assignments_and_duration.sql
 */
+SELECT 
+  assignment.day,
+  count(assignments.*) AS number_of_assignments,
+  sum(assignments.duration) AS duration
+FROM
+  assignments
+GROUP BY
+  assignmnets.day;
