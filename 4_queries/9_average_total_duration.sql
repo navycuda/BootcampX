38d1e5665d5ae8a74732a58e3a9e00ab7ad9da81
NavyCuda <navysupra@gmail.com>
2022-08-23 17:52:32 -0700

    Type out correct solution

--- 4_queries/9_average_total_duration.sql
+++ 4_queries/9_average_total_duration.sql
@@ -1,6 +1,5 @@
 /*
 \i 4_queries/9_average_total_duration.sql
-*/
 SELECT
   cohorts.name AS name,
   max(assistance_requests.completed_at - assistance_requests.started_at) AS average_assistance_time
@@ -30,3 +29,24 @@ HAVING avg(assistance_requests.completed_at - assistance_requests.started_at) >=
 )
 ORDER BY
   average_assistance_time;
+*/
+
+SELECT
+  avg(total_duration) as average_total_duration
+FROM (
+  SELECT
+    cohorts.name as cohort,
+    sum(assistance_requests.completed_at-assistance_requests.started_at) AS total_duration
+  FROM
+    assistance_requests
+  JOIN
+    students
+    ON assistance_requests.student_id = students.id
+  JOIN
+    cohorts
+    ON students.cohort_id = cohorts.id
+  GROUP BY
+    cohorts.name
+  ORDER BY
+    total_duration
+) AS total_durations;
