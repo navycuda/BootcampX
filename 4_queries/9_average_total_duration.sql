3f393dd582a190b0e856511df2b78872d4c867dd
NavyCuda <navysupra@gmail.com>
2022-08-23 17:44:43 -0700

    Another Attempt
    	It seems I did a few attempts without committing.  Got some
    	help from a fellow student for the solution.

--- 4_queries/9_average_total_duration.sql
+++ 4_queries/9_average_total_duration.sql
@@ -1,3 +1,32 @@
 /*
 \i 4_queries/9_average_total_duration.sql
 */
+SELECT
+  cohorts.name AS name,
+  AVG(assistance_requests.completed_at - assistance_requests.started_at) AS average_assistance_time
+FROM
+  cohorts
+JOIN
+  students
+  ON cohorts.id = students.cohort_id
+JOIN
+  assistance_requests
+  ON students.id = assistance_requests.student_id
+GROUP BY
+  cohorts.name
+HAVING avg(assistance_requests.completed_at - assistance_requests.started_at) >= ALL(
+  SELECT 
+    avg(assistance_requests.completed_at - assistance_requests.started_at)
+  FROM
+    assistance_requests
+  JOIN
+    students
+    ON assistance_requests.student_id = students.id
+  JOIN
+    cohorts
+    ON students.cohort_id = cohorts.id
+  GROUP BY
+    cohorts.name
+)
+ORDER BY
+  average_assistance_time;
