c1e6fa03868951d4659ee81c4e51be781fa27c55
NavyCuda <navysupra@gmail.com>
2022-08-23 19:25:09 -0700

    Fix type

--- 4_queries/B_total_assignments_and_duration.sql
+++ 4_queries/B_total_assignments_and_duration.sql
@@ -2,7 +2,7 @@
 \i 4_queries/B_total_assignments_and_duration.sql
 */
 SELECT 
-  assignment.day,
+  assignments.day,
   count(assignments.*) AS number_of_assignments,
   sum(assignments.duration) AS duration
 FROM
