954d14457d47eec4c5735c71568f125920bbf567
NavyCuda <navysupra@gmail.com>
2022-08-24 23:38:58 -0700

    Deep dive into error

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,27 +8,27 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT
-    students.id,
-    students.name,
-    cohorts.name AS cohort
-  FROM
-    students
-  JOIN
-    cohorts
-    ON students.cohort_id = cohorts.id
-  LIMIT
-    5
-  ;
-`)
-  .then(response => {
-    // console.log(response.rows);
-    response.rows.forEach((user) => {
-      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
-    });
-  })
-  .catch(error => console.error('query error', error.stack));
+// pool.query(`
+//   SELECT
+//     students.id,
+//     students.name,
+//     cohorts.name AS cohort
+//   FROM
+//     students
+//   JOIN
+//     cohorts
+//     ON students.cohort_id = cohorts.id
+//   LIMIT
+//     5
+//   ;
+// `)
+//   .then(response => {
+//     // console.log(response.rows);
+//     response.rows.forEach((user) => {
+//       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+//     });
+//   })
+//   .catch(error => console.error('query error', error.stack));
 
 const query = `
   SELECT
